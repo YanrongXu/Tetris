7c20bf6161d4070cceb98e3b182166609de29f56
Danny Xu <xuyanrong@me.com>
2021-05-08 20:33:26 -0400

    Styling the game

---
+++ constants.js
@@ -0,0 +1,13 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
+const canvas = document.getElementById('board')
+const ctx = canvas.getContext('2d')
+
+// Calculate size of canvas from constants
+ctx.canvas.width = COLS * BLOCK_SIZE
+ctx.canvas.height = ROWS * BLOCK_SIZE
+
+// Scale blocks
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
